54ac68a5ae30121b024ae243d75e95c8cec826c3
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-23 20:10:32 +0200

    some changes

--- MY-JS/script.js
+++ MY-JS/script.js
@@ -1,1 +1,8 @@
-console.log('connected');
+console.log(document);
+
+const Menu_Bar = document.querySelector('#open-nav-menu');
+const NavigationMenu = document.querySelector('header nav .wrapper');
+
+Menu_Bar.addEventListener('click', function () {
+  NavigationMenu.classList.add('nav-open');
+});
