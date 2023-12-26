569035b3f1d678987498e2647c76af0d5e6e4510
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-26 20:29:09 +0200

    some changes

--- MY-JS/src/main.js
+++ MY-JS/src/main.js
@@ -1,2 +1,8 @@
 console.log('Main JS Here');
 
+// Hero Section
+const greetingText = 'Good Morning';
+const weatherCondition = 'Sunny';
+const userLocation = 'New York';
+let temperature = 22.8673;
+let weatherText = `The weather is cloudy in London and it's 22 °C outside`
