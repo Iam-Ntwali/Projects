1b8496abc38fe3dc9f661818364cf6c42f84d636
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-26 20:38:03 +0200

    some changes

--- MY-JS/src/main.js
+++ MY-JS/src/main.js
@@ -1,8 +1,17 @@
 console.log('Main JS Here');
 
 // Hero Section
+// HTML Element
+const greetingTextEl = document.querySelector('#greeting');
+const weatherEl = document.querySelector('p#weather');
+
+// variables
 const greetingText = 'Good Morning';
 const weatherCondition = 'Sunny';
 const userLocation = 'New York';
-let temperature = 22.8673;
-let weatherText = `The weather is cloudy in London and it's 22 °C outside`
+let temperature = 22.86;
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)} °C outside`
+
+// Assigning Values
+greetingTextEl.textContent = greetingText;
+weatherEl.textContent = weatherText;
