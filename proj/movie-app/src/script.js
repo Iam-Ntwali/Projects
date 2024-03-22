89eb7bb43572e5928f33f6dfd61d949e87bbbfe2
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-03-23 00:43:45 +0200

    some changes

--- proj/movie-app/src/script.js
+++ proj/movie-app/src/script.js
@@ -1,6 +1,12 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=4fd223a643800f435e6ed8a4fd71dc1e'
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=4fd223a643800f435e6ed8a4fd71dc1e';
+const IMGPATH = 'hhtps://image.tmdb/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie&api_key=4fd223a643800f435e6ed8a4fd71dc1e&query=';
 
-console.log(APILINK);
-const IMGPATH = 'hhtps://image.tmdb/t/p/w1280'
-const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie&api_key=4fd223a643800f435e6ed8a4fd71dc1e'
+// HTML Elements
+const display = '';
+const form = document.getElementById('form');
+const search = document.getElementById('movie-query');
+
+// return movie
+
 
