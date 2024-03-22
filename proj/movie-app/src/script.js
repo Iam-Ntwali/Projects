ac9695daa2a5248409e67251388cc08ac92c1a72
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-03-23 00:22:41 +0200

    added APIs

---
+++ proj/movie-app/src/script.js
@@ -0,0 +1,6 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=4fd223a643800f435e6ed8a4fd71dc1e'
+
+console.log(APILINK);
+const IMGPATH = 'hhtps://image.tmdb/t/p/w1280'
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie&api_key=4fd223a643800f435e6ed8a4fd71dc1e'
+
