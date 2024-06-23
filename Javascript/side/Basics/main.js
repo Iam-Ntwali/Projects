808b7b6316135bd9a9f41ee487d7661e12804609
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-23 20:02:00 +0200

    some changes

--- Javascript/side/Basics/main.js
+++ Javascript/side/Basics/main.js
@@ -37,81 +37,81 @@ for (data in students) {
 }
 
 // challenge
-// let firstFavNum = 24;
-// let secondFavNum = 36;
+let firstFavNum = 24;
+let secondFavNum = 36;
 
-// console.log(firstFavNum + secondFavNum);
-// console.log(firstFavNum - secondFavNum);
-// console.log(firstFavNum * secondFavNum);
-// console.log(firstFavNum / secondFavNum);
-// console.log(firstFavNum % secondFavNum);
-// console.log(firstFavNum ** 2);
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** 2);
 //
 
-// let user = 'Ntwali';
-// const occupation = 'Web Developer';
+let user = 'Ntwali';
+const occupation = 'Web Developer';
 
-// let whatDoesUserDo = user + ' is a ' + occupation;
-// console.log(whatDoesUserDo);
-// console.log(whatDoesUserDo.slice(0));
+let whatDoesUserDo = user + ' is a ' + occupation;
+console.log(whatDoesUserDo);
+console.log(whatDoesUserDo.slice(0));
 
-// let favActorFirstName = 'Nicolas';
-// let favActorLastName = 'Cage';
-// let actorFullName = favActorFirstName + ' ' + favActorLastName;
-// console.log(actorFullName);
+let favActorFirstName = 'Nicolas';
+let favActorLastName = 'Cage';
+let actorFullName = favActorFirstName + ' ' + favActorLastName;
+console.log(actorFullName);
 
-// let uppercase = actorFullName.toUpperCase();
-// console.log(uppercase);
-// actorFullName += ' best movie is Firefox';
-// console.log(actorFullName);
+let uppercase = actorFullName.toUpperCase();
+console.log(uppercase);
+actorFullName += ' best movie is Firefox';
+console.log(actorFullName);
 
 // CONVERT TYPES
-// let money = 50; // type of money is  a number
-// console.log(money, 'is a', typeof money);
+let money = 50; // type of money is  a number
+console.log(money, 'is a', typeof money);
 
-// money = String(money); // convert a number to a string
-// console.log(money, 'is now a', typeof money);
+money = String(money); // convert a number to a string
+console.log(money, 'is now a', typeof money);
 
-// money = parseInt(money); // convert a string to a number
-// console.log(money, 'is now a', typeof money, 'again 🙂');
+money = parseInt(money); // convert a string to a number
+console.log(money, 'is now a', typeof money, 'again 🙂');
 
-// money = Number(money); // convert a string to a number
-// console.log('or using the constructor Number()', money, 'is still converted to a', typeof money);
+money = Number(money); // convert a string to a number
+console.log('or using the constructor Number()', money, 'is still converted to a', typeof money);
 
 // CONTROL FLOW (if, if else, switch case) //
-// const password = '@u9e-0';
-// const login = 'logged in successfully';
-// if (password.length >= 8) {
-//   console.log(login);
-// } else {
-//   console.warn('password is too short', '\n Your passsword is', password.length, 'characters long');
-// }
+const password = '@u9e-0';
+const login = 'logged in successfully';
+if (password.length >= 8) {
+  console.log(login);
+} else {
+  console.warn('password is too short', '\n Your passsword is', password.length, 'characters long');
+}
 
-// let workingDays
+let workingDays
 
-// switch (workingDays) {
-//   case 1:
-//     console.log('Monday');
-//     break;
-//   case 2:
-//     console.log('Tuesday');
-//     break;
-//   case 3:
-//     console.log('Wednesday');
-//     break;
-//   case 4:
-//     console.log('Thursday');
-//     break;
-//   case 5:
-//     console.log('Friday');
-//     break;
-//   case 6:
-//     console.log('Saturday');
-//     break;
-//   case 7:
-//     console.log('Sunday');
-//     break;
-//   default:
-//     console.log('Not a valid day');
-//     break;
-// }
+switch (workingDays) {
+  case 1:
+    console.log('Monday');
+    break;
+  case 2:
+    console.log('Tuesday');
+    break;
+  case 3:
+    console.log('Wednesday');
+    break;
+  case 4:
+    console.log('Thursday');
+    break;
+  case 5:
+    console.log('Friday');
+    break;
+  case 6:
+    console.log('Saturday');
+    break;
+  case 7:
+    console.log('Sunday');
+    break;
+  default:
+    console.log('Not a valid day');
+    break;
+}
